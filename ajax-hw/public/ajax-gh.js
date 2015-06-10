5fa604e0a66bc2e88e5abe7db60bc96464077b8b
Ghaea <ghaea.tam@gmail.com>
2015-06-10 16:02:49 -0400

    added org... has my picture instead of org picture

--- ajax-hw/public/ajax-gh.js
+++ ajax-hw/public/ajax-gh.js
@@ -40,14 +40,13 @@ $(document).on('ready', function() {
 	  		
 	  		$(".gh-pages").append(htmlString)
 	  	})
-
 	  	
 	  }
 	})
 
 // Hard mode
 
-	var starTempFunction = Handlebars.compile( $("#ghRepoTemplate").html() )
+	var starTempFunction = Handlebars.compile( $("#profileTemplate").html() )
 
 	$.ajax({ 
 	  url: "https://api.github.com/users/ghaea/starred",
@@ -61,5 +60,29 @@ $(document).on('ready', function() {
 	  	$("#starred").text(data.length.toString())
   	
 	  }
+	})
+
+	var orgTempFunction = Handlebars.compile( $("#profileTemplate").html() )
+
+	$.ajax({ 
+	  url: "https://api.github.com/users/ghaea/orgs",
+	  method: "GET",
+	  data: {
+	    access_token: "3b2e77f921315bbd9d39a0ec8267ff75107c7aff"
+	  },
+	  success: function(data) { 
+	  	console.log(data)
+
+	  	_.each(data, function(org){
+	  		var htmlString = orgTempFunction({
+		  		org_url: "https://api.github.com/users/ghaea/orgs",
+		  		avatar_url: "https://avatars.githubusercontent.com/u/12126092?v=3"
+		  		})
+	  		$(".orgAvatar").append(htmlString)
+
+	  		})
+	  	
+		}
+	  	
 	})
 })
